996235672e194cae5bc799c05da7f4fa8b09ff5d
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-05-10 18:01:53 -0500

    agregando models y tests

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,9 +1,12 @@
+const User = require("./../../app/models/User")
+
 describe("Suite de pruebas #2", () => {
     test('Case 1: Probando mi spiderverse', () => {
        //Escribimos el codigo que queremos usar tal cual
-      const newUser = new User(1829333,"BrianOrihuelaP","Brian","I´ma sotfware engineer","10/05/2022","10/05/2022")
-    
-      expect(newUser.name).toBe("BrianOrihuelaP");
-       
+      const newUser = new User(1829333,"BrianOrihuelaP","Brian","Engineer","10/05/2022","10/05/2022")
+      expect(newUser.id).toBe(1829333);
+      expect(newUser.username).toBe("BrianOrihuelaP");
+      expect(newUser.name).toBe("Brian");
+      expect(newUser.bio).toBe("Engineer");
     });
   })
