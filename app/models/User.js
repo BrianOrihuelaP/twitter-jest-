940b7b2cd28b75bfbfa0da40cbf11393e1afd7ab
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-05-10 18:17:45 -0500

    Agregando requerimientos (1 y 2)

--- app/models/User.js
+++ app/models/User.js
@@ -4,8 +4,9 @@ class user{
         this.username = username 
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdate = lastUpdate
+        this.dateCreated = new Date()
+        this.lastUpdate = new Date() 
+        //este constructor llama a la clase DATE la cual toma la fecha y hora del S.O
     }
 }
 
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,12 +1,20 @@
 const User = require("./../../app/models/User")
 
-describe("Suite de pruebas #2", () => {
-    test('Case 1: Probando mi spiderverse', () => {
+describe("Suite de pruebas #1", () => {
+    test('Requerimiento 1 : Creacion del modelo User', () => {
        //Escribimos el codigo que queremos usar tal cual
-      const newUser = new User(1829333,"BrianOrihuelaP","Brian","Engineer","10/05/2022","10/05/2022")
+      const newUser = new User(1829333,"BrianOrihuelaP","Brian","Engineer")
       expect(newUser.id).toBe(1829333);
       expect(newUser.username).toBe("BrianOrihuelaP");
       expect(newUser.name).toBe("Brian");
       expect(newUser.bio).toBe("Engineer");
     });
+
+    test('Requerimiento 2 : Datos de tipo fecha (user) ', () => {
+      //Escribimos el codigo que queremos usar tal cual
+     const newUser = new User(1829333,"BrianOrihuelaP","Brian","Engineer")
+     expect(newUser.id).not.toBeUndefined(); 
+     //este predicado nos dice que lo esperado debe de estar si o si definido, si no esta definido no pasa la prueba
+   });
+
   })
