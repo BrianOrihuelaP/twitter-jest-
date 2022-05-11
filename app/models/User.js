0bcdc0beb13627733250fd0d76206e9fe5eb145f
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-05-10 20:57:57 -0500

    Agregando setters y corrigiendo getters

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,5 @@
 class user{
-    constructor(id,username,name,bio,dateCreated,lastUpdate){
+    constructor(id,username,name,bio){
         this.id = id
         this.username = username 
         this.name = name
@@ -7,6 +7,38 @@ class user{
         this.dateCreated = new Date()
         this.lastUpdate = new Date() 
         //este constructor llama a la clase DATE la cual toma la fecha y hora del S.O
+    }
+
+    get getName(){
+        return this.name;
+    }
+
+    get getId(){
+        return this.id;
+    }
+
+    get getUsername(){
+        return this.username;
+    }
+
+    set setUsername(username){
+        this.username = username;
+    }
+
+    get getBio(){
+        return this.bio;
+    }
+
+    set setBio(bio){
+        this.bio = bio;
+    }
+
+    get getdateCreated(){
+        return this.dateCreated;
+    }
+
+    get getLastUpdate(){
+        return this.lastUpdate;
     }
 }
 
